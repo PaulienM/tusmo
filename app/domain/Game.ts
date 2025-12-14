a15e954b744d97e7d86ba8cb92246e8111b02418
PaulienM <paulien.dev@proton.me>
2025-12-14 14:54:42 +0100

    feat: gestion des différents status de partie (gagnée, perdue, non commencée et en cours)

--- app/domain/Game.ts
+++ app/domain/Game.ts
@@ -1,6 +1,8 @@
 import {GameStatusEnum} from "./GameStatusEnum";
 
 export class Game {
+    static MAX_ATTEMPTS = 6;
+
     solution: string;
     status: GameStatusEnum = GameStatusEnum.NOT_STARTED;
     guesses: string[] = [];
@@ -12,6 +14,10 @@ export class Game {
 
     public addGuess(guess: string): void {
         this.guesses.push(guess);
+        this.status = GameStatusEnum.IN_PROGRESS;
+        if (this.guesses.length >= Game.MAX_ATTEMPTS) {
+            this.status = GameStatusEnum.LOST;
+        }
         if (guess === this.solution) {
             this.status = GameStatusEnum.WON;
         }
--- test/domain/game.test.ts
+++ test/domain/game.test.ts
@@ -2,9 +2,24 @@ import {test, describe, expect} from 'vitest'
 import {Game} from "../../app/domain/Game";
 import {GameStatusEnum} from "../../app/domain/GameStatusEnum";
 
-describe('Test de la classe Game', () => {
+describe('Test des status de la classe Game', () => {
+    test('Partie non commencée si aucun essai n\'a été fait', () => {
+        const game = new Game('solution', []);
+        expect(game.status).toBe(GameStatusEnum.NOT_STARTED)
+    })
+
+    test('Partie en cours si un essai a été fait', () => {
+        const game = new Game('solution', ['sabotage']);
+        expect(game.status).toBe(GameStatusEnum.IN_PROGRESS);
+    })
+
     test('Partie gagnée si le premier mot correspond à la solution', () => {
         const game = new Game('solution', ['solution']);
-        expect(game.status).toBe(GameStatusEnum.WON)
+        expect(game.status).toBe(GameStatusEnum.WON);
+    })
+
+    test('Partie perdue si on fait 6 essais faux', () => {
+        const game = new Game('solution', ['sabotage', 'saccager', 'sacrifié', 'sagacité', 'sagement', 'saignant']);
+        expect(game.status).toBe(GameStatusEnum.LOST);
     })
 })
